4d485926af7e96496ff8c26df49ecc1e45eb1350
mkuliberda <m.kuliberda@s4t.com.pl>
2021-02-19 18:11:58 +0100

    strings and system.out fun

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,7 +1,14 @@
 package com.example.helloworld;
 
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        boolean test = true;
+        String message = "Hello world!";
+        message = message.concat(" You're awesome.");
+        System.out.println(message);
+        Pump p1 = new Pump();
+        test = p1.run();
+        System.out.print(test);
     }
 }
---
+++ src/com/example/helloworld/Pump.java
@@ -0,0 +1,9 @@
+package com.example.helloworld;
+
+public class Pump {
+    boolean running=false;
+
+    boolean run(){
+        return false;
+    }
+}
