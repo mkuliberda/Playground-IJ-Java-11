7ffde51c5d1559e8210a9d3da3aa671433a81b3f
mkuliberda <m.kuliberda@s4t.com.pl>
2021-03-01 16:49:41 +0100

    Intellij's functions and objects playing around

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,14 +1,48 @@
 package com.example.helloworld;
-
+import java.time.*;
 
 public class HelloWorld {
+
+    public boolean isTestBool() {
+        return testBool;
+    }
+
+    public void setTestBool(boolean testBool) {
+        this.testBool = testBool;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private boolean testBool = true;
+    private String message = "Hello world!";
+
+
     public static void main(String[] args) {
-        boolean test = true;
-        String message = "Hello world!";
-        message = message.concat(" You're awesome.");
-        System.out.println(message);
+        HelloWorld hello = new HelloWorld();
+
+        hello.setMessage("Hello world!".concat(" You're awesome."));
+        System.out.println(hello.getMessage());
         Pump p1 = new Pump();
-        test = p1.run();
-        System.out.print(test);
+        hello.setTestBool(p1.run());
+        System.out.println(hello.isTestBool());
+        System.out.println(p1.stop().run());
+
+        Boolean running = false;
+        System.out.println(running.toString());
+        Character myName = 0x65;
+        System.out.println(myName.charValue());
+
+        ExtendedPump p2 = new ExtendedPump();
+        p2.backward();
+
+        Object o = new Object();
+        System.out.println(ExtendedPump.encodeStatus());
+
     }
 }
--- src/com/example/helloworld/Pump.java
+++ src/com/example/helloworld/Pump.java
@@ -2,8 +2,16 @@ package com.example.helloworld;
 
 public class Pump {
     boolean running=false;
+    String state = "unknown";
 
-    boolean run(){
-        return false;
-    }
+    boolean run() {state = "running"; return false;}
+    void forward(){state = "running forward"; running = true;}
+    Pump stop(){state = "stopped"; running = false; return this;}
+    void printState() {System.out.println(state);}
+}
+
+class ExtendedPump extends Pump{
+
+    void backward() {state = "running backward"; running = true; printState();}
+    static int encodeStatus(){return 0x89;}
 }
